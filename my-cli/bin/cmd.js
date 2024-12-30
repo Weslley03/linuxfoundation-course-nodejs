3004a2eb70b08ecd087ea26cb38173c9ae475569
oweslley <wesleymoraeswv03@hotmail.com>
2024-12-30 12:25:18 -0300

    added `program library` for best cli app

--- my-cli/bin/cmd.js
+++ my-cli/bin/cmd.js
@@ -1,44 +1,68 @@
 #!/usr/bin/env node
-import got from 'got';
-const API = `http://localhost:3000`;
+import { Command } from 'commander';
+import { update, add, listCategoryItems, listCategories } from '../src/utils.js';
 
-const usage = (msg = `Back office for My App`) => {
-  console.log(`\n${msg}\n`);
-  console.log(`usage: cmd <ID> <amount >`);
-};
+//create a new Command Program
+const program = new Command();
 
-//get the arguments from the command line.
-const argv = process.argv.slice(2);
+//create a new program
+program
+  .name(`my-cli`)
+  .description(`Back office for My App`)
+  .version(`1.0.0`);
 
-//if there are no arguments, show the usage and exit.
-if(argv.length < 2) {
-  usage();
-  process.exit(1);
-};
+//create a command for adding a updating order
+program
+  .command('update')
+  .argument('<ID>', 'Order ID')
+  .argument("<AMOUNT>", "Order Amount")
+  .action(async (id,amount) => await update(id,amount));
 
-//deconstruct the arguments into variables
-const [ argId, argAmount ] = argv;
 
-//check if the amount ' is a number.
-const amount  = parseInt(argAmount);
+//create a command for listing categories by IDs
+program
+.command("add")
+.description("add Product by ID to a Category")
+.argument("<CATEGORY>", "Product Category")
+.argument("<ID>", "Product ID")
+.argument("<NAME>", "Product Name")
+.argument("<AMOUNT>", "Product RRP")
+.argument("[INFO...]", "Product Info")
+.action(
+  async (category, id, name, amount, info) =>
+    await add(category, id, name, amount, info)
+);
 
-//if there are no arguments, show the usage and exit.
-if(isNaN(amount )) {
-  usage(`Error: <AMOUNT> must be a number`);
-  process.exit(1);
-};
+//create a command for listing categories
+program
+  .command("list")
+  .description("List categories")
+  .argument("[CATEGORY]", "category to list IDs for")
+  .option("-a, --all", "list all categories")
+  .action(async (args, opts) => {
+    if (args && opts.all)
+      throw new Error("cannot specify both category and 'all'");
+    if (opts.all || args === "all") {
+      listCategories();
+    } else if (args === "confectionery" || args === "electronics") {
+      await listCategoryItems(args);
+    } else {
+      throw new Error("Invalid category specified");
+    }
+  });
 
-//update the order with a given ID
+  //set the action to be executed when the command is run
+program
+  .action(async (args, opts) => {
+  if (args && opts.all)
+    throw new Error("Cannot specify both category and 'all'");
+  if (opts.all || args === "all") {
+    listCategories();
+  } else if (args === "confectionery" || args === "electronics") {
+    await listCategoryItems(args);
+  } else {
+    throw new Error("Invalid category specified");
+  }
+});
 
-try{
-  //use GOT to make a POST request to the API
-  await got.post(`${API}/orders/${argId}`, {
-    json: { amount },
-  })
-  //log the result to the console
-  console.log(`order ${argId} updated with amount ${amount}`);
-}catch(err) {
-  //if there is an error, log it to the console and exit
-  console.log(err.message);
-  process.exit(1);
-};
+program.parse();
---
+++ my-cli/src/utils.js
@@ -0,0 +1,129 @@
+import { got } from "got";
+import { displayAmount, displayID, displayInfo, displayName, displaySuccess, displayText, displayTimestamp } from "./displays.js";
+
+//set the API URL
+const API = "http://localhost:3000";
+
+//set the categories
+const categories = ["confectionery", "electronics"];
+
+//log the usage of the command to the console 
+export const log = (msg) => {
+  console.log(`\n${msg}\n`);
+};
+
+//log the error to the console
+export const error = (msg) => {
+  console.error(`\n${msg}\n`);
+};
+
+//update the order with the given ID
+export async function update(id, amount) {
+  log(`${displayTimestamp()}`);
+  log(
+    `${displayInfo(`Updating Order`)} ${displayID(id)} ${displayText(
+      "with amount"
+    )} ${displayAmount(amount)}`
+  );
+  try {
+    if (isNaN(+amount)) {
+      error("Error: <AMOUNT> must be a number");
+      process.exit(1);
+    }
+    //use GOT to make a POST request to the API
+    await got.post(`${API}/orders/${id}`, {
+      json: { amount: +amount },
+    });
+    //log the result to the console
+    log(
+      `${displaySuccess()} ${displayText("Order")} ${displayID(
+       id
+      )} ${displayText("updated with amount")} ${displayAmount(amount)}`
+    );
+  } catch (err) {
+    //if there is an error, log it to the console and exit
+    console.log(err.message);
+    process.exit(1);
+  }
+};
+
+export async function add(...args) {
+  let [ category, id, name, amount, info ] = args;
+  log(`${displayTimestamp()}`);
+  log(
+    `${displayInfo(`Request to add item to category`)} ${displayCategory(
+      category
+    )}`
+  );
+  log(
+    `${displayText("Adding item")} ${displayID(id)} ${displayText(
+      "with amount"
+    )} ${displayAmount(`$${amount}`)}`
+  );
+  try{
+    if (isNaN(+amount)) {
+      error(`error: <AMOUNT> must be a number`);
+      process.exit(1);
+    };
+    //use GOT to make a POST request to the API
+    await got.post(`${API}/${category}`, {
+      json: {
+        id,
+        name,
+        rrp: +amount,
+        info: info.join(" "),
+      },
+    });
+    //log the result to the console
+    log(
+      `${displaySuccess("Product Added! :")} ${displayID(id)} ${displayName(
+        name
+      )} ${displayText("has been added to the")} ${displayCategory(
+        category
+      )} ${displayText("category")}`
+    );
+  }catch(err) {
+    //if there is an error, log it to the console and exit
+    error(err.message);
+    process.exit(1);
+  };
+};
+
+//list the categories
+export function listCategories() {
+  log(displayTimestamp());
+  log(displayInfo("Listing Categories"));
+  try {
+    // Loop through the categories and log them to the console
+    log(displayText("Categories received from API:"));
+    for (const cat of categories) log(displayCategory(cat));
+  } catch (err) {
+    //if there is an error, log it to the console and exit
+    error(err.message);
+    process.exit(1);
+  }
+};
+
+//list the IDs for the given category
+export async function listCategoryItems(category) {
+  log(displayTimestamp());
+  log(`${displayInfo(`List IDs`)}`);
+  try {
+    // Use GOT to make a GET request to the API
+    const result = await got(`${API}/${category}/`).json();
+    // Log the result to the console
+    log(`${displaySuccess("IDs received from API:")}`);
+    for (const item of result) {
+      log(`
+        ${displayKey("ID:")}\t${displayID(item.id)}
+        ${displayKey(`Name:`)}\t${displayName(item.name)}
+        ${displayKey("RRP:")}\t${displayRRP(item.rrp)}
+        ${displayKey("Product Info:")}\n\t${displayText(item.info)}
+      `);
+    }
+  } catch (err) {
+    //if there is an error, log it to the console and exit
+    console.log(err.message);
+    process.exit(1);
+  };
+};
